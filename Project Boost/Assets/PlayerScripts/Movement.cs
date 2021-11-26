4e850461575211fac21cc4aa1fb7d0a50ab2f3d7
XionsFlame <69725309+XionsFlame@users.noreply.github.com>
2021-11-26 14:19:09 -0800

    Rigid Body, Basic Physics
    Added Thrust, basic level blocks, and rigidbodies.

---
+++ Project Boost/Assets/PlayerScripts/Movement.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField]
+    float force;
+
+    [SerializeField]
+    float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Rotation(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Rotation(Vector3.back);
+        }
+    }
+
+    private void Rotation(Vector3 direction)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(direction * rotateSpeed * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * force * Time.deltaTime);
+        }
+    }
+}
